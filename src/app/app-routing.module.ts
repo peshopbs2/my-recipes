9359c1e4962b08b9d0d0f6b7a6eae1129c739080
peshopbs2 <peshopbs2@gmail.com>
2021-04-16 16:56:44 +0300

    added navigation to recipes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeComponent } from './recipe/recipe.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'recipes', component: RecipeComponent},
+  {path: 'shopping-list', component: ShoppingListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  
+}
--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes : Recipe[] = [
+    new Recipe("Таратор", "Лятна супа", "https://upload.wikimedia.org/wikipedia/commons/1/19/TaratorBg.jpg"),
+    new Recipe("Шопска салата", "Салата с домати, краставици и сирене", "http://balkanskaskara.net/wp-content/uploads/2018/07/02_Shopska.jpg"),
+    new Recipe("Странджанка", "Филия с кайма", "https://speedyburger.bg/sburger/uploads/2016/11/strandzhanka.jpg"),
+    new Recipe("Таратор", "Лятна супа", "https://upload.wikimedia.org/wikipedia/commons/1/19/TaratorBg.jpg"),
+    new Recipe("Шопска салата", "Салата с домати, краставици и сирене", "http://balkanskaskara.net/wp-content/uploads/2018/07/02_Shopska.jpg"),
+    new Recipe("Странджанка", "Филия с кайма", "https://speedyburger.bg/sburger/uploads/2016/11/strandzhanka.jpg"),
+    new Recipe("Таратор", "Лятна супа", "https://upload.wikimedia.org/wikipedia/commons/1/19/TaratorBg.jpg"),
+    new Recipe("Шопска салата", "Салата с домати, краставици и сирене", "http://balkanskaskara.net/wp-content/uploads/2018/07/02_Shopska.jpg"),
+    new Recipe("Странджанка", "Филия с кайма", "https://speedyburger.bg/sburger/uploads/2016/11/strandzhanka.jpg"),    new Recipe("Таратор", "Лятна супа", "https://upload.wikimedia.org/wikipedia/commons/1/19/TaratorBg.jpg"),
+    new Recipe("Шопска салата", "Салата с домати, краставици и сирене", "http://balkanskaskara.net/wp-content/uploads/2018/07/02_Shopska.jpg"),
+    new Recipe("Странджанка", "Филия с кайма", "https://speedyburger.bg/sburger/uploads/2016/11/strandzhanka.jpg")
+
+  ];
+
+  message : String = "баничка";
 
   constructor() { }
 
